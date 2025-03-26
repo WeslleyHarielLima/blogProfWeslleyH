02fd59b325c781a5ce75bfd7519018c10592ec37
weslley <weslleyhariellima@gmail.com>
2025-03-26 09:36:13 -0400

    Criação de modulo introdutorio de construção do index

--- blog/app/controllers/home_controller.rb
+++ blog/app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @nome = "USER MASTER"
+    # @nomes = ["Alice", "Bob", "Carlos", "Diana"]
 
-    @nome= "Weslleynome"
-    @nomes = ["Alice", "Bob", "Carlos", "Diana"]
-
-
+    @devs = [
+      { nome: 'Weslley',  dev: 'Ruby on Rails' },
+      { nome: 'André',    dev: 'Next.js' },
+      { nome: 'Kaike',    dev:'Mysql'}
+    ]
   end
-
 end
